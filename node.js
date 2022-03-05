ebb85f6ddfd9e766c954dde7016d877082e0039e
Fernando <fernandolopezreina@gmail.com>
2022-03-05 19:01:44 +0100

    d

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {    
-        origin: "https://chatio-ferlr.netlify.app/",    
+        origin: "https://chatio-ferlr.netlify.app",    
 }})
 
 var num_usr = 0;
